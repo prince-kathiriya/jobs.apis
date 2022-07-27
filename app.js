fca0cd5cb41038b0c59402440a8e22c208814714
prince-172 <prince172.rejoice@gmail.com>
2022-07-27 14:25:54 +0530

    Initial commit from ubuntu

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const start = async () => {
 	try {
--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,7 @@ const register = async (req, res) => {
 	const token = user.createJWT();
 	res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
+
 const login = async (req, res) => {
 	const { email, password } = req.body;
 	if (!email || !password) {
